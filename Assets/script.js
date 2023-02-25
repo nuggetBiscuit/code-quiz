fa1e31c1a082d0c27c3af1969f36b55c6740e3d3
Khuzema <maslam002@citymail.cuny.edu>
2023-02-24 23:54:10 -0500

    Added 3 code questions.More to come

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+const quiz = [
+    {
+        question: "A loop that never ends is referred to as a(n)_________",
+      choices: ["A while loop", "B infinite loop", "C recursive loop", "D for loop"],
+      answer: 1
+    },
+    {
+      question: "Finding and solving errors in the source code is called what?",
+      choices: ["A desk checking", "B testing", "C debugging", "D decoding"],
+      answer: 2
+    },
+    {
+      question: "What does HTML stand for?",
+      choices: ["A HyperText and links Markup Language", "B HighText Machine Language", "C HyperText Markup Language", "D none of these"],
+      answer: 2
+    }
+  ];
