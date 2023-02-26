dfcef8a298966e394d3e56c09ceb4614f6f811be
Khuzema <maslam002@citymail.cuny.edu>
2023-02-26 02:32:00 -0500

    Fixed start button and added working timer

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,34 @@
+function startQ(){
+  // Show the submit button and hide the start button
+  document.getElementById("submit").style.display = "block";
+  document.getElementById("startButton").style.display = "none";
+  document.getElementById("paraText").style.display = "none";
+  
+  // Show the question and choices elements
+  document.getElementById("question").style.display = "block";
+  document.getElementById("choices").style.display = "inline-block";
+  
+}
+
+function submitAnswer() {
+  document.getElementById("submit").style.display = "block";
+  
+}
+
 const quiz = [
     {
       question: "A loop that never ends is referred to as a(n)_________",
-      choices: ["A while loop", "B infinite loop", "C recursive loop", "D for loop"],
+      choices: ["A. while loop", "B. infinite loop", "C. recursive loop", "D. for loop"],
       answer: 1
     },
     {
       question: "Finding and solving errors in the source code is called what?",
-      choices: ["A desk checking", "B testing", "C debugging", "D decoding"],
+      choices: ["A. desk checking", "B. testing", "C. debugging", "D. decoding"],
       answer: 2
     },
     {
       question: "What does HTML stand for?",
-      choices: ["A HyperText and links Markup Language", "B HighText Machine Language", "C HyperText Markup Language", "D none of these"],
+      choices: ["A. HyperText and links Markup Language", "B. HighText Machine Language", "C. HyperText Markup Language", "D. none of these"],
       answer: 2
     }
   ];
@@ -46,6 +63,9 @@ const quiz = [
       score++;
     } else {
       timeLeft -= 10;
+      // Updates timer to reflect the subtracted time for wrong answers
+      timeElement.textContent = timeLeft;
+
     }
     currentQuestionIndex++;
     if (currentQuestionIndex === quiz.length) {
@@ -61,9 +81,26 @@ const quiz = [
     setInterval(updateTime, 1000);
   }
   
+  function updateTime() {
+    if (timeLeft > 0) {
+      timeLeft--;
+      timeElement.textContent = timeLeft;
+    } else {
+      endQuiz();
+    }
+  }
 
-  
-
+  function endQuiz() {
+    quizContainer.style.display = 'none';
+    const name = prompt('Enter your name:');
+    // If there is a highScores value, it will parse it as a JSON object. Otherwise, it will output an empty array
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    highScores.push({ name, score });
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    showHighScores();
+  }
   
 
   
